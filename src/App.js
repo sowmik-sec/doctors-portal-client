dcffc3c876c9c4f4b878c0190263bdc59030ef54
sowmik-sec <sowmik.sec@gmail.com>
2023-03-27 18:17:26 +0600

    setup react router dom layout and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./Routes/Routes/Routes";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
