ce021342af9c305949792370603638af4db269b3
sowmik-sec <sowmik.sec@gmail.com>
2023-03-28 12:12:27 +0600

    margin added

--- src/Pages/Home/Services/Service.js
+++ src/Pages/Home/Services/Service.js
@@ -3,7 +3,7 @@ import React from "react";
 const Service = ({ service }) => {
   const { img, title, description } = service;
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl mx-auto">
       <figure>
         <img src={img} alt="service" />
       </figure>
