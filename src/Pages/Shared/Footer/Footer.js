0ff53d7d726dab033e8af18fd1e7d36fae3f8403
sowmik-sec <sowmik.sec@gmail.com>
2023-03-28 15:19:25 +0600

    footer created

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import photo from "../../../assets/images/footer.png";
 
 const Footer = () => {
-  return <div>this is footer</div>;
+  return (
+    <footer
+      className="p-10"
+      style={{
+        background: `url(${photo})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="footer">
+        <div>
+          <span className="footer-title">Services</span>
+          <Link className="link link-hover">Branding</Link>
+          <Link className="link link-hover">Design</Link>
+          <Link className="link link-hover">Marketing</Link>
+          <Link className="link link-hover">Advertisement</Link>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <Link className="link link-hover">About us</Link>
+          <Link className="link link-hover">Contact</Link>
+          <Link className="link link-hover">Jobs</Link>
+          <Link className="link link-hover">Press kit</Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link className="link link-hover">Terms of use</Link>
+          <Link className="link link-hover">Privacy policy</Link>
+          <Link className="link link-hover">Cookie policy</Link>
+        </div>
+      </div>
+      <div className="text-center mt-32">
+        <p>Copyright © 2023 - All right reserved by Doctor's Portal Ltd</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
