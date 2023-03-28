a98ffac77a4f21cab6c689110d92ad6115d813aa
sowmik-sec <sowmik.sec@gmail.com>
2023-03-28 12:31:26 +0600

    banner image width changed and padding reduced

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,8 +5,8 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div className="hero banner-bgi">
-      <div className="hero-content flex-col lg:flex-row-reverse py-52 px-5">
-        <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt="" />
+      <div className="hero-content flex-col lg:flex-row-reverse py-16 px-5">
+        <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
         <div>
           <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p className="py-6">
