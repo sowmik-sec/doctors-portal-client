ceb7408ca9c7c053b4c148513b4cf744bd06ec4b
sowmik-sec <sowmik.sec@gmail.com>
2023-03-28 12:54:25 +0600

    make appointment section added

---
+++ src/Pages/Home/MakeAppointment/MakeAppointment.js
@@ -0,0 +1,38 @@
+import React from "react";
+import doctor from "../../../assets/images/doctor.png";
+import appointment from "../../../assets/images/appointment.png";
+
+const MakeAppointment = () => {
+  return (
+    <section className="mt-32" style={{ background: `url(${appointment})` }}>
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src={doctor}
+            className="-mt-32 lg:w-1/2 md:block hidden rounded-lg shadow-2xl"
+            alt=""
+          />
+          <div>
+            <p className="text-lg text-primary font-bold">Appointment</p>
+            <h1 className="text-4xl text-white font-bold">
+              Make an appointment Today
+            </h1>
+            <p className="py-6 text-white">
+              It is a long established fact that a reader will be distracted by
+              the readable content of a page when looking at its layout. The
+              point of using Lorem Ipsumis that it has a more-or-less normal
+              distribution of letters,as opposed to using 'Content here, content
+              here', making it look like readable English. Many desktop
+              publishing packages and web page
+            </p>
+            <button className="btn btn-primary text-white bg-gradient-to-r from-primary to-secondary">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MakeAppointment;
