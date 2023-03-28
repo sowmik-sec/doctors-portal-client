4b29d7a37e05789e761918fe897c304162269ec8
sowmik-sec <sowmik.sec@gmail.com>
2023-03-28 18:11:25 +0600

    appointment banner created using react day picker

---
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import chair from "../../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import { format } from "date-fns";
+
+const AppointmentBanner = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  return (
+    <header className="my-6">
+      <div className="hero">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
+          <div className="mr-6">
+            <DayPicker
+              mode="single"
+              selected={selectedDate}
+              onSelect={setSelectedDate}
+            />
+            <p>You have selected date: {format(selectedDate, "PP")}</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AppointmentBanner;
