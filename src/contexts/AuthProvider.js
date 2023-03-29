4e847359434744bc9a41bf4ab554229e686e38bc
sowmik-sec <sowmik.sec@gmail.com>
2023-03-29 17:18:00 +0600

    state observer added

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,8 +1,9 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
@@ -10,12 +11,19 @@ const auth = getAuth(app);
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState("");
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
   const signIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unSubscribe;
+  }, []);
   const authInfo = { createUser, signIn };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
