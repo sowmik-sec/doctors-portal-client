a4f90d4220de5ed2f5fe0e43cd5ee1efd11e07bf
sowmik-sec <sowmik.sec@gmail.com>
2023-04-01 10:36:23 +0600

    load all users on the dashboard page

---
+++ src/Pages/Dashboard/AllUsers/AllUsers.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useQuery } from "react-query";
+
+const AllUsers = () => {
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users");
+      const data = await res.json();
+      return data;
+    },
+  });
+  return (
+    <div>
+      <h2 className="text-3xl">All Users</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Favorite Color</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>Blue</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,13 +2,19 @@ import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider";
+import useToken from "../../hooks/useToken";
 
 const Login = () => {
   const { signIn, googleSignIn } = useContext(AuthContext);
   const [loginError, setLoginError] = useState("");
+  const [loginUserEmail, setLoginUserEmail] = useState("");
+  const [token] = useToken(loginUserEmail);
   const location = useLocation();
   const navigate = useNavigate();
   const from = location.state?.from?.pathname || "/";
+  if (token) {
+    navigate(from, { replace: true });
+  }
   const {
     register,
     formState: { errors },
@@ -19,9 +25,7 @@ const Login = () => {
     setLoginError("");
     signIn(data.email, data.password)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
-        navigate(from, { replace: true });
+        setLoginUserEmail(data.email);
       })
       .catch((err) => {
         setLoginError(err.message);
