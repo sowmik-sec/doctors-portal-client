8949d5736b1af37a3fa5e65a556aa04f90e53885
sowmik-sec <sowmik.sec@gmail.com>
2023-04-03 11:10:50 +0600

    adding loader

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,14 +1,19 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import CheckoutForm from "./CheckoutForm";
+import Loading from "../../Shared/Loading/Loading";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
 
 const Payment = () => {
   const booking = useLoaderData();
+  const navigation = useNavigation();
   const { treatment, price, appointmentDate, slot } = booking;
+  if (navigation.state === "loading") {
+    return <Loading />;
+  }
   return (
     <div>
       <h3 className="text-3xl">Payment for {treatment}</h3>
