21c55fa20b5c03a08ff59582a15ee16b20a89ae5
sowmik-sec <sowmik.sec@gmail.com>
2023-04-03 11:07:07 +0600

    store payment data to db

--- src/Pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment/MyAppointment.js
@@ -50,7 +50,7 @@ const MyAppointment = () => {
                       </Link>
                     )}
                     {booking.price && booking.paid && (
-                      <span className="text-primary">Paid</span>
+                      <span className="text-green-500">Paid</span>
                     )}
                   </td>
                 </tr>
--- src/Pages/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -9,7 +9,7 @@ const CheckoutForm = ({ booking }) => {
   const [clientSecret, setClientSecret] = useState("");
   const stripe = useStripe();
   const elements = useElements();
-  const { price, email, patient } = booking;
+  const { price, email, patient, _id } = booking;
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     fetch("http://localhost:5000/create-payment-intent", {
@@ -59,9 +59,29 @@ const CheckoutForm = ({ booking }) => {
       return;
     }
     if (paymentIntent.status === "succeeded") {
-      setSuccess("Congrats! your payment completed");
-      setTransactionId(paymentIntent.id);
       // store payment info in the database
+      const payment = {
+        price,
+        transactionId: paymentIntent.id,
+        email,
+        bookingId: _id,
+      };
+      fetch(`http://localhost:5000/payments`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(payment),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.insertedId) {
+            setSuccess("Congrats! your payment completed");
+            setTransactionId(paymentIntent.id);
+          }
+        });
     }
     setProcessing(false);
     console.log("paymentIntent", paymentIntent);
